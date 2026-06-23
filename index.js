c8cb045bc302b715f589e04b45b9b546b9915c8f
Iwhite67 <alexis_z@hotmail.fr>
2026-06-23 19:10:01 +0200

    fix: serve admin.html at /admin route

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'admin.html'));
+});
+
 const PORT = process.env.PORT || 8181;
 app.listen(PORT, () => {
   log.info('server', 'Démarrage', {
